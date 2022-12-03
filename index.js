f345724e02c4f823795a2da0fe2ef8add7b933df
matin ghaedi <ghaedidev@gmail.com>
2022-12-03 21:53:30 +0330

    add writing part

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
-let links=document.querySelectorAll('.link');
-let listOfLink=[...links];
-console.log(listOfLink);
-listOfLink.map(link=>{
-    link.addEventListener('click',()=>{
-        listOfLink.forEach(l=>l.classList.remove('active'));
-        link.classList.toggle('active');
+let links = document.querySelectorAll('.link');
+let section = document.querySelectorAll('section')
+
+function activeLink() {
+    links.forEach((item) =>
+        item.classList.remove('active'));
+    this.classList.add('active');
+}
+links.forEach((item) =>
+    item.addEventListener('click', activeLink));
+
+window.onscroll = () => {
+    section.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+        console.log(id);
+        if (top >= offset && top < offset + height) {
+            links.forEach((item) => {
+                item.classList.remove('active');
+                if (item.getAttribute('href') == '#' + id) {
+                    item.classList.add('active');
+                }
+            });
+        }
     })
-})
+};
